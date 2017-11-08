604a16342e8a5a93cfe45e7dc204b81f437060fe
86352 <86352@MHS-C212-08.mentorschools.org>
2017-11-08 11:35:46 -0500

    Thurday's Weather NY

--- Weather3/WeatherTech.py
+++ Weather3/WeatherTech.py
@@ -1,1 +1,2 @@
 print ("The weather today in New York is cloudy with a high of 50, and a low of 41.")
+print ("Thursdays weather in New York City is mostly cloudy with a high of 55, and a low of 37")
