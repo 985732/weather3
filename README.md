2a74964f14c41f1b7b38165ae01b3aa9898b1729
86067 <86067@MHS-C212-27.mentorschools.org>
2017-11-08 11:37:21 -0500

    Added Friday and made changes to Thursday

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # weather3
+# Hi
--- Weather3/WeatherTech.py
+++ Weather3/WeatherTech.py
@@ -1,2 +1,3 @@
 print ("The weather today in New York is cloudy with a high of 50, and a low of 41.")
-print ("Thursdays weather in New York City is mostly cloudy with a high of 55, and a low of 37")
+print ("Thursday's weather in New York City is mostly cloudy with a high of 55, and a low of 37")
+print ("Friday's weather in New York is partly cloudy with a high of 55 and low of 37")
