91e5f2e638b89e9185eacce133f88cf598b619c7
86067 <86067@MHS-C212-27.mentorschools.org>
2017-11-08 11:48:04 -0500

    added comments

--- Weather3/WeatherTech.py
+++ Weather3/WeatherTech.py
@@ -1,7 +1,7 @@
-print ("The weather today in New York is cloudy with a high of 50, and a low of 41.")
-print ("Thursday's weather in New York City is mostly cloudy with a high of 55, and a low of 37")
-print ("Friday's weather in New York is partly cloudy with a high of 55 and low of 37")
-print ("Saturday's weather in New York is Mostly Sunny with a high of 43 and a low of 35.")
-print ("Sunday's weather in New York is mostly sunny with a high of 52, and a low of 46")
-print ("Monday's weather in New York is rainy with a high of 53 and low of 46")
-print ("Tuesday's weather in New York is Partly Cloudy with a high of 55 and a low of 42.")
+print ("The weather today in New York is cloudy with a high of 50, and a low of 41.")# display today's weather
+print ("Thursday's weather in New York City is mostly cloudy with a high of 55, and a low of 37")# display Thursday's weather
+print ("Friday's weather in New York is partly cloudy with a high of 55 and low of 37")# display Friday's weather
+print ("Saturday's weather in New York is Mostly Sunny with a high of 43 and a low of 35.")# display Saturday's weather
+print ("Sunday's weather in New York is mostly sunny with a high of 52, and a low of 46")# display Sunday's weather
+print ("Monday's weather in New York is rainy with a high of 53 and low of 46")# display Monday's weather
+print ("Tuesday's weather in New York is Partly Cloudy with a high of 55 and a low of 42.")# display Tuesday's weather
